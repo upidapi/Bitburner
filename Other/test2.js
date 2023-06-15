13419f0972e7b8e6d8ba589e253ad59a88b8fc8d
upidapi <videw@icloud.com>
2023-06-16 00:42:06 +0200

    save

--- Other/test2.js
+++ Other/test2.js
@@ -1,16 +1,49 @@
 /** @param {NS} ns */
 export async function main(ns) {
+  doc = globalThis["document"]
 
+  //const g = greeting("test");
+  ns.disableLog("ALL");
+  ns.clearLog();
+  ns.print("test")
+  let x = 0
+  const g = greeting(x);
+  console.log(g.props);
+  g.style = "wdith:100%;height:100%";
+  ns.printRaw(g);
+  ns.print("a")
+  while (true) {
+    if (g.props.countUpB) g.props.countUpB();
+    await ns.sleep(0);
+  }
+}
 
-    for (let i = 0; i < 100; i++) {
-        ns.run("HybridShotgunBatcher/ThreadScripts/Weaken.js", 1, "n00dles", i)
-        // let s = Date.now()
-        // let r = ns.getWeakenTime("n00dles")
+function greeting(name) {
+  return React.createElement(
+    TestComp,
+    { className: 'greeting', toWhat: 0 }
+  );
+}
 
-        // await ns.weaken("n00dles")
+class TestComp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: this.props.toWhat,
+    };
+  }
 
-        // let d = Date.now() - s
+  componentDidMount() {
+    this.props.countUpB = this.countUp.bind(this);
+  }
 
-        // ns.tprint(d - r, " ", d, " ", r)
-    }
+  countUp() {
+    this.setState((state, props) => ({
+      counter: state.counter + 1,
+    }));
+  }
+
+  render() {
+    return React.createElement('h1', null, `Hello ${this.state.counter}`);
+  }
 }
