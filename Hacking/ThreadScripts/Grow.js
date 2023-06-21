d193052cd36700c8c140b03616e0fd43b101c191
upidapi <videw@icloud.com>
2023-06-21 15:55:59 +0200

    removed old code

--- Hacking/ThreadScripts/Grow.js
+++
@@ -1,9 +0,0 @@
-/**
- * thease files are for "multithreading"
- */
-
-/** @param {NS} ns */
-export async function main(ns) {
-    await ns.grow(ns.args[0])
-    ns.tprintf("b")
-  }
--- Hacking/ThreadScripts/Hack.js
+++
@@ -1,5 +0,0 @@
-/** @param {NS} ns */
-export async function main(ns) {
-    await ns.hack(ns.args[0])
-    ns.tprintf("a")
-  }
